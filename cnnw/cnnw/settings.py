6f47bbe8dc22d1e1ac575aff6116243a93cb738c
Matheus Frata <msfrata@hotmail.om>
2017-06-20 20:14:20 -0300

    migrations for offline postgres db

--- cnnw/cnnw/settings.py
+++ cnnw/cnnw/settings.py
@@ -78,8 +78,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'cnnw_db',
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'mfrata',
+        'PASSWORD': 'sudofrata',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
