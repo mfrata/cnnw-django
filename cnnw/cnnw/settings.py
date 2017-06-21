331e593f357cfdfd2b212d9c645f0346d6a30ad5
Matheus Frata <msfrata@hotmail.om>
2017-06-21 14:04:52 -0300

    Basic form at login page

--- cnnw/cnnw/settings.py
+++ cnnw/cnnw/settings.py
@@ -78,8 +78,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'cnnw_db',
-        'USER': 'mfrata',
-        'PASSWORD': 'sudofrata',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
