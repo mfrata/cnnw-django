8c0acf48920e25791e37cbea278af328b679d08a
Matheus Frata <msfrata@hotmail.om>
2017-06-21 13:37:25 -0300

    first version of index page with bootstrap

--- cnnw/cnnw/urls.py
+++ cnnw/cnnw/urls.py
@@ -20,6 +20,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url(r'^webclient/', include('webclient.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout')
+    url(r'^$', include('webclient.urls'))
 ]
