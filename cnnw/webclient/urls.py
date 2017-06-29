68645cb90886ed3da03cd868e31d7c4205b30f84
Ricardo Maurici <ricardo.maurici@hotmail.com>
2017-06-29 15:55:49 -0300

    login authentication

--- cnnw/webclient/urls.py
+++ cnnw/webclient/urls.py
@@ -3,8 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.login, name='login'),
+    url(r'^$', views.login_user, name='login_user'),
+    url(r'^login/$', views.login_user, name='login_user'),
     url(r'^search/$', views.search, name='search'),
     url(r'^config/$', views.config, name='config'),
     url(r'^dashboard/$', views.dashboard, name='dashboard'),
 ]
+
--- cnnw/webclient/views.py
+++ cnnw/webclient/views.py
@@ -1,17 +1,41 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
+from django.template import RequestContext
+from django.urls import reverse
 
-def login(request):
-	context = {}
-	return render(request, 'webclient/login.html', context)
+def login_user(request):
+	logout(request)
+	username = ''
+	password = ''
+	form = request.POST or None
+	print(request)
+	if request.POST:
+		username = request.POST['email']
+		password = request.POST['password']
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			if user.is_active:
+				print('alg')
+				login(request, user)
+				return HttpResponseRedirect(reverse('search'))
+		return render(request, 'webclient/login.html', {
+            'error_message': "Email ou senha incorretos.",
+        })
+	return render(request, 'webclient/login.html')
 
+@login_required(login_url='/webclient/login/')
 def search(request):
 	context = {}
 	return render(request, 'webclient/search.html', context)
 
+@login_required(login_url='/webclient/login/')
 def config(request):
 	context = {}
 	return render(request, 'webclient/config.html', context)
 
+@login_required(login_url='/webclient/login/')
 def dashboard(request):
 	context = {}
 	return render(request, 'webclient/dashboard.html', context)
