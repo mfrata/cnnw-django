0adec091b6617bfb532634c0511330c3ae5f0aab
Ricardo Maurici <ricardo.maurici@hotmail.com>
2017-06-30 16:56:15 -0300

    removed description

--- cnnw/webclient/models.py
+++ cnnw/webclient/models.py
@@ -2,19 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Company(models.Model):
-	name = models.CharField(max_length=30)
+	name = models.CharField(max_length=50)
 
 class Person(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=50)
 
 class Category(models.Model):
     name = models.CharField(max_length=30)
-    description = models.CharField(max_length=100)
     companies = models.ManyToManyField(Company)
     persons = models.ManyToManyField(Person)
 
 class Tag(models.Model):
     name = models.CharField(max_length=30)
-    description = models.CharField(max_length=100)
     user = models.ForeignKey(User)
     categories = models.ManyToManyField(Category)
