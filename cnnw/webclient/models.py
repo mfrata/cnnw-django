d434580d6dca55bdf2e4057411338733f1e1f85c
Ricardo Maurici <ricardo.maurici@hotmail.com>
2017-07-03 16:48:00 -0300

    add str self

--- cnnw/webclient/models.py
+++ cnnw/webclient/models.py
@@ -3,16 +3,28 @@ from django.contrib.auth.models import User
 
 class Company(models.Model):
 	name = models.CharField(max_length=50)
+	
+	def __str__(self):
+		return self.name
 
 class Person(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+		return self.name
 
 class Category(models.Model):
     name = models.CharField(max_length=30)
     companies = models.ManyToManyField(Company)
     persons = models.ManyToManyField(Person)
 
+    def __str__(self):
+		return self.name
+
 class Tag(models.Model):
     name = models.CharField(max_length=30)
     user = models.ForeignKey(User)
     categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+		return self.name
