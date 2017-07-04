f75dcc36064a1f34ed3e5bf3b04600035c1c259a
Matheus Frata <msfrata@hotmail.om>
2017-07-04 19:24:51 -0300

    heroku requirements

--- README.md
+++
@@ -1,7 +0,0 @@
-python 3.6.0
-django 3.11
-django_rest_framework 3.6.3
-postgres >9 
-pip install coreapi, pygments, markdown - tenta instalar e olhar a documentacao da API, se nao der certo instale esses
-http://localhost:8000/webclient/ - sistema
-http://localhost:8000/webclient/api/v1/docs/
--- cnnw/cnnw/settings.py
+++ cnnw/cnnw/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@*z52((ivpvnm_9g!5+x3+@4=oi7x2uj7gk56ywgyzrenyi#y9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
