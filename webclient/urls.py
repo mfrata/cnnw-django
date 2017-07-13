dd0b2e0530f1300a28bd80f9bb58f694148cc701
Ricardo Maurici <ricardo.maurici@hotmail.com>
2017-07-13 09:01:55 -0300

    reset password button

--- webclient/urls.py
+++ webclient/urls.py
@@ -1,13 +1,14 @@
-from django.conf.urls import include, url
-from . import views
-
 from django.contrib.auth import views as auth_views
+from django.conf.urls import include, url
+from django.views.generic import RedirectView
+from . import views
 
 urlpatterns = [
     url(r'^$', views.login_user, name='login_user'),
     url(r'^login/$', views.login_user, name='login_user'),
-    url(r'^admin/$', views.login_user, name='login_user'),
-    url(r'^home/$', views.login_user, name='login_user'),
+    url(r'^admin/$', RedirectView.as_view(url='/')),
+    url(r'^home/$', RedirectView.as_view(url='/')),
+    url(r'^accounts/login/$', RedirectView.as_view(url='/')),
     url(r'^search/$', views.search, name='search'),
     url(r'^config/$', views.config, name='config'),
     url(r'^dashboard/$', views.dashboard, name='dashboard'),
@@ -18,6 +19,4 @@ urlpatterns = [
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         auth_views.password_reset_confirm, name='password_reset_confirm'),
     url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
-    url(r'^accounts/login/', views.login_user, name='login_user'),
-
 ]
